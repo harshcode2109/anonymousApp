e30412c32772b58c21e5ef92f5bf93ce3f53488b
harshcode2109 <156992122+harshcode2109@users.noreply.github.com>
2025-07-05 11:13:08 +0530

    user

---
+++ backend/routes/user.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { getUser } from "../controllers/user.controller.js";
+
+
+
+const router = express.Router();
+router.get("/getuser/:userId",getUser)
+
+
+
+
+
+export default router;
